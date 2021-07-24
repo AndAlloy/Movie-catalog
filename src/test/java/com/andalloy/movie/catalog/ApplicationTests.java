e87016338ab7166e9ec1eec218bb25fb5298a03c
AndAlloy <kormilo121@gmail.com>
2021-07-24 13:06:28 +0300

    initial

---
+++ src/test/java/com/andalloy/movie/catalog/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andalloy.movie.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
